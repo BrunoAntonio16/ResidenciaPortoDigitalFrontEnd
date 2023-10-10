1873b5e2d132c32d321b2b6c11ee10ba06c67d11
Bruno Antonio <brunoantonio0210@gmail.com>
2023-10-10 02:29:44 -0300

    Adição da pasta layouts, que irá conter todos os componentes referentes a Navbar e Rodapé da Aplicação

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>SimileSystem</h1>
+     <h1>SimleSystem</h1>
     </div>
   );
 }
---
+++ src/layouts/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <div>
+            <a>Home</a>
+            <img class="logo" src="apple-touch-icon.png" alt="logo"/>
+            <a>Sobre</a>
+        </div>  
+    )
+}
+
+export default Navbar
