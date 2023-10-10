5d3d9f637f69aa34518b4da6d0b88be2041882a0
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2023-10-10 17:03:10 -0300

    Instalação do react-router-dom e criação de arquivo routesjs

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route } from "react-router-dom"
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" elment={''}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes
