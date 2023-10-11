4f388e3d358e175c84fe1ab2e90c18ec954807a7
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2023-10-11 14:51:25 -0300

    Commit forçadogit checkout main

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,17 +1,22 @@
 import logo from '../images/apple-touch-icon.png'
+import styles from './Navbar.module.css'
 
 function Navbar() {
     return (
         <header className={styles.navbar} >
-            <a href="#">Home</a>
-            <img src={logo} alt="SmileSystem" />
-            <a href="#">Sobre</a>
-            <nav>
-                <a href="#">Listas</a>
-                <a href="#">Alocações</a>
-                <a href="#">Horaríos</a>
-                <a href="#">Presença</a>
-            </nav>
+            <div className={styles.navone}>
+                <a href="#">Home</a>
+                <img src={logo} alt="SmileSystem" />
+                <a href="#">Sobre</a>
+            </div>
+            <div className={styles.navtwo} >
+                <nav>
+                    <a href="#">Listas</a>
+                    <a href="#">Alocações</a>
+                    <a href="#">Horaríos</a>
+                    <a href="#">Presença</a>
+                </nav>
+            </div>
         </header>
     )
 }
