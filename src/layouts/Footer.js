a87983f63539ea7f6ac7d4bd6de818f85b702cce
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2023-10-17 15:27:22 -0300

    Estilização do Footer + Adição de Fontes

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,10 +1,18 @@
 import styles from './Footer.module.css'
+import logo from '../images/apple-touch-icon.png'
 
 function Footer() {
     return (
         <footer className={styles.footerConteiner} >
-            <div className={styles.footerBottom}>
-                <p>Copyright &copy;2023 </p>
+            <div className={styles.socialIcons}>
+                <a href='#'><i className="fa-brands fa-facebook"></i></a>
+                <a href='#'><i className="fa-brands fa-instagram"></i></a>
+                <a href='#'><i className="fa-brands fa-twitter"></i></a>
+                <a href='#'><i className="fa-brands fa-youtube"></i></a>
+            </div>
+            <div className={styles.footerBottom} >
+                <img className={styles.logo} src={logo} alt="SmileSystem" />
+                <p>&copy; 2023 SmileSystem</p>
             </div>
         </footer>
     )
