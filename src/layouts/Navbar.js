a85524d8f42c2e4a87b52b54c0be946eceaff770
unknown <brunoantonio0210@gmail.com>
2023-11-09 10:59:14 -0300

    Create-Navbar-Layout

---
+++ src/layouts/Navbar.js
@@ -0,0 +1,21 @@
+import './Navbar.css';
+
+function Navbar() {
+    return(
+        <div className="navbar">
+            <div className="navbar-logo">
+                SmileSystem
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">a</a></li>
+                    <li><a href="#">a</a></li>
+                    <li><a href="#">a</a></li>
+                    <li><a href="#">a</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
