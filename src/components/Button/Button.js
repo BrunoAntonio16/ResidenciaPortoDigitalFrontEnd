2b2002a76725d7519d0e1999eb0c800aabfdc2e7
unknown <brunoantonio0210@gmail.com>
2023-11-10 17:44:36 -0300

    Create new component Button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import './Buttom.css';
+
+function Button(props) {
+    return(
+        <div className="button">
+            <button>Entrar</button>
+        </div>
+    );
+}
+
+export default Button;
