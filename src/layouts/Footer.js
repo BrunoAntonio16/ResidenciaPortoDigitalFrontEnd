98929ee8f6cd7de52e8d9895917a85f96aaa29a2
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2023-11-17 19:57:32 -0300

    footer-bootstrap-implementation

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,21 +1,34 @@
-import './Footer.css'; 
+import "./Footer.css";
 
 function Footer() {
     return (
-        <footer className="footer">
-            <nav>
-                <ul>
-                    <li><a href="a">a</a></li>
-                    <li><a href="a">a</a></li>
-                    <li><a href="a">a</a></li>
-                    <li><a href="a">a</a></li>
-                </ul>
-            </nav>
-            <div className="footer-copy">
-                <p>Copyright &copy;2023; SmileSystem</p>
+      <footer className="p-2 pb-0 fixed-bottom">
+        <div className="container text-dark text-center">
+          <div className="row d-flex justify-content-center">
+            <div className="text-center">
+              <ul className="list-unstyled list-inline">
+                <li className="list-inline-item">
+                  <a href="#" className="text-dark px-2">
+                    Whatsapp
+                  </a>
+                  <a href="#" className="text-dark px-2">
+                    Instagram
+                  </a>
+                  <a href="#" className="text-dark px-2">
+                    Discord
+                  </a>
+                </li>
+              </ul>
             </div>
-        </footer>
-    )
-}
-
-export default Footer;
+          </div>
+          <div className="row d-flex justify-content-center">
+            <div>
+              <p>Copyright &copy;2023; SmileSystem</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
