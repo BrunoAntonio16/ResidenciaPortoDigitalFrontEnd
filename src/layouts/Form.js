7a1754636b2d3764741609639a610846cdc17dd6
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2023-11-17 20:02:29 -0300

    form-bootstrap-implementation

--- src/layouts/Form.js
+++ src/layouts/Form.js
@@ -1,30 +1,39 @@
-import './Form.css';
+import "./Form.css";
 
 function Form() {
-    return(
-        <> 
-         <div className="form-box2">
-            <div className="form">
-                <div className="form-h1">
-                    <h1>Login</h1>
-                </div>
-                <form>
-                    <div className="form-email">
-                        <label>Email:</label>
-                        <input type="text"/>
-                    </div>
-                    <div className="form-senha">
-                        <label>Senha:</label>
-                        <input type="password"/>
-                    </div>
-                </form>
-                <div className="form-button">
-                    <button>Entrar</button>
-                </div>
-            </div> 
-         </div>     
-        </>
-    );
+  return (
+    <>
+      <div className="container-fluid">
+        <form className="mx-auto">
+          <h1 className="text-center">Login</h1>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">
+              Senha
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Entrar
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Form;
