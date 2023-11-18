9feeca9c47cb4ab73884b35f5e35cdd419c8d55d
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2023-11-17 21:22:36 -0300

    list-bootstrap-implementation

--- src/layouts/Lista.js
+++ src/layouts/Lista.js
@@ -2,92 +2,83 @@ import './Lista.css';
 
 function Lista(){
     return(
-        <div className="table">
-            <div className="table-title">
-                <h1>Lista</h1>
-            </div>
-            <div className="table-menu-itens">
-                <nav>
-                    <ul>
-                        <li><a href="#">Alunos</a></li>
-                        <li><a href="#">Mentores</a></li>
-                        <li><a href="#">Empresas</a></li>
-                        <li><a href="#">Faculdades</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div className="table-filter">
-                <input placeholder="Pesquisar"/>
-            </div>
-            <div className="table-body-itens">
-                <table>
+        <>
+            <div className="container-fluid mt-3">
+                <div className="row table-menu-itens me-auto">
+                    <nav>
+                        <ul className='mb-0 py-3 px-2'>
+                            <li><a href="#">Aluno</a></li>
+                            <li><a href="#">Mentor</a></li>
+                            <li><a href="#">Empresa</a></li>
+                            <li><a href="#">Faculdade</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <table class="table table-responsive-sm table-bordered table-hover text-center">
                     <thead>
-                        <tr>
-                            <th>@</th>
-                            <th>@</th>
-                            <th>@</th>
-                            <th>@</th>
-                            <th>@</th>
-                            <th>@</th>
-                            <th>@</th>
+                        <tr className="principal-line">
+                            <th scope="col">Id</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Faculadade</th>
+                            <th scope="col">Turma</th>
+                            <th scope="col">Turno</th>
+                            <th scope="col">Squad</th>
+                            <th scope="col">Empresa</th>
+                            <th scope="col">Mentor</th>
+                            <th scope="col">Functions</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                        <td>
-                            <button>Editar</button>
-                            <button>Remover</button>
-                        </td>
+                            <th scope="row">1</th>
+                            <td>Bruno Antonio</td>
+                            <td>brunoantonio0210@gmail.com</td>
+                            <td>FICR</td>
+                            <td>3-A</td>
+                            <td>Noite</td>
+                            <td>32</td>
+                            <td>Bidweb</td>
+                            <td>Robson José</td>
+                            <td className='functions'>
+                                <a href='#'><i class="fa-solid fa-square-plus"></i></a>
+                                <a href='#'><i class="fa-solid fa-trash"></i></a>
+                            </td>
                         </tr>
                         <tr>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                        <td>
-                            <button>Editar</button>
-                            <button>Remover</button>
-                        </td>
+                        <th scope="row">1</th>
+                            <td>Bruno Antonio</td>
+                            <td>brunoantonio0210@gmail.com</td>
+                            <td>FICR</td>
+                            <td>3-A</td>
+                            <td>Noite</td>
+                            <td>32</td>
+                            <td>Bidweb</td>
+                            <td>Robson José</td>
+                            <td className='functions'>
+                                <a href='#'><i class="fa-solid fa-square-plus"></i></a>
+                                <a href='#'><i class="fa-solid fa-trash"></i></a>
+                            </td>
                         </tr>
                         <tr>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                        <td>
-                            <button>Editar</button>
-                            <button>Remover</button>
-                        </td>
-                        </tr>
-                        <tr>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                            <td>#</td>
-                        <td>
-                            <button>Editar</button>
-                            <button>Remover</button>
-                        </td>
+                        <th scope="row">1</th>
+                            <td>Bruno Antonio</td>
+                            <td>brunoantonio0210@gmail.com</td>
+                            <td>FICR</td>
+                            <td>3-A</td>
+                            <td>Noite</td>
+                            <td>32</td>
+                            <td>Bidweb</td>
+                            <td>Robson José</td>
+                            <td className='functions'>
+                                <a href='#'><i class="fa-solid fa-square-plus"></i></a>
+                                <a href='#'><i class="fa-solid fa-trash"></i></a>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-            <div className="table-button">
-                <button>Novo usuario</button>
-            </div>
-        </div>
+        </>
     );
 }
 
