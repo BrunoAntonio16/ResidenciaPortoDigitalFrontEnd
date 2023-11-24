53775238cf90098100f2784a85dd2f829217d3dd
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2023-11-24 20:46:51 -0300

    add-AppRoutes-in-App.js

--- src/App.js
+++ src/App.js
@@ -1,98 +1,19 @@
+
 import NavBar from './components/NavBar/NavBar';
 //import { Form } from 'react-router-dom';
 //import Table from './components/Table/Table';
 import Lista from './layouts/Lista';
-import FormLogin from './components/FormLogin/FormLogin';
+import FormLogin from './components/FormLogin';
 import Filtro from './layouts/Filtro';
 import Navbar from './layouts/Navbar';
 import Footer from './layouts/Footer';
 import Form from './layouts/Form';
-
-const meusItems = [
-  {
-    route: "",
-    text: "Home"
-  },
-
-  {
-    route: "",
-    text: "Info"
-  },
-
-  {
-    route: "",
-    text: "About"
-  },
-
-  {
-    route: "",
-    text: "Functions"
-  }
-
-];
-
-const Alunos = [
-  {
-    aluno: "Bruno Antonio",
-    turma: "Única",
-    turno: "Noite",
-    email: "brunoantonio0210@gmail.com"
-  },
-
-  {
-    aluno: "Bruno Antonio",
-    turma: "Única",
-    turno: "Noite",
-    email: "brunoantonio0210@gmail.com"
-  }
-];
-
-const meusIcons = [
-  {
-    link: "fa-brands fa-instagram"
-  },
-
-  {
-    link: "fa-brands fa-whatsapp"
-  },
-
-  {
-    link: "fa-brands fa-linkedin"
-  },
-
-  {
-    link: "fa-brands fa-square-x-twitter"
-  }
-];
-
-const meusInputs = [
-  {
-    type: "text",
-    placeholder: "Email",
-  },
-
-  {
-    type: "password",
-    placeholder: "Senha"
-  }
-];
-
-const icons = [
-  {
-    class: "fa-solid fa-user"
-  },
-
-  {
-    class: "fa-solid fa-lock"
-  }
-];
-
+import FormTitle from './components/FormTitle';
+import AppRoutes from "./routes";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Lista />
-      <Footer />
+          <AppRoutes/>
     </div>
   );
 }
